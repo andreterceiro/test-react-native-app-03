843893a9138216104e1a4db154b60ac9726f0a00
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-28 11:27:52 -0300

    feat: files related to routing

---
+++ test-app-3/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native'
+
+export default function Routes() {
+    return <View>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+        <Text>oi</Text>
+    </View>
+}
