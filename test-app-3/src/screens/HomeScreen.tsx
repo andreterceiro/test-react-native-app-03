7f92497b65b292b8d566d335136952cbdd353602
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2026-01-28 13:14:19 -0300

    feat: adjusting the height of HomeScreen

--- test-app-3/src/screens/HomeScreen.tsx
+++ test-app-3/src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import {Text, View} from 'react-native'
 
 export default function HomeScreen() {
-    return (<View style={{backgroundColor: 'red', height: '50%'}}>
+    return (<View style={{backgroundColor: 'red', height: '100%'}}>
         <Text>Home screen</Text>
     </View>);
 }
